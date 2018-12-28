96c682380aaf181d7a67a24e9d79bf8e8daf3ec6
jake <JakeSiegers@users.noreply.github.com>
2018-12-28 13:23:27 -0600

    More work on menu block class

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Object = System.Object;
 using Random = UnityEngine.Random;
 
 public class Menu : MonoBehaviour {
@@ -37,79 +39,76 @@ public class Menu : MonoBehaviour {
 
 		//gameObject.GetComponent<Renderer>().material.DOColor(Color.green, 10);
 
+		var menuBlock1 = MenuBlock.createMenuBlock(new MenuBlock.ButtonConfig("This is some text", 0, 0.5f),transform);
+		var menuBlock2 = MenuBlock.createMenuBlock(new MenuBlock.ButtonConfig("Some More Text", 0, 0),transform);
+		var menuBlock3 = MenuBlock.createMenuBlock(new MenuBlock.ButtonConfig("Even More Text", 0, -0.5f),transform);
 		
-		gameObject.transform.localScale = Vector3.zero;
-		gameObject.transform.position = new Vector3(0,7,0);
-		gameObject.transform.rotation = Quaternion.Euler(180,-270,0);
-		
-		var cube = Instantiate(Resources.Load<GameObject>("Prefabs/MenuBlock"));
-		cube.transform.SetParent(gameObject.transform, false);
-		cube.transform.localPosition = new Vector3(0,0,-0.4f);
-		cube.transform.localEulerAngles = Vector3.zero;
-		cube.transform.localScale = Vector3.one;
+		transform.localScale = Vector3.zero;
+		transform.position = new Vector3(0,7,0);
+		transform.rotation = Quaternion.Euler(180,-270,0);
 
-		gameObject.transform.DOMove(menuPosition,2).SetEase(Ease.InOutExpo);		
-		gameObject.transform.DOScale(Vector3.one,2).SetEase(Ease.InOutExpo);
-		gameObject.transform.DORotate(Vector3.zero, 2).SetEase(Ease.InOutExpo);
+		transform.DOMove(menuPosition,2).SetEase(Ease.InOutExpo);		
+		transform.DOScale(Vector3.one,2).SetEase(Ease.InOutExpo);
+		transform.DORotate(Vector3.zero, 2).SetEase(Ease.InOutExpo);
 
 		//InvokeRepeating ("flip", 0, 2); 
 
 	}
 
 	void flip(){
-		gameObject.transform.DORotate(new Vector3(Random.Range(0,360), Random.Range(0,360),Random.Range(0,360)),1).SetEase(Ease.OutExpo);
+		transform.DORotate(new Vector3(Random.Range(0,360), Random.Range(0,360),Random.Range(0,360)),1).SetEase(Ease.OutExpo);
 	}
 	
 	// Update is called once per frame
-	private void Update () {
+	private void Update() {
 		//_time = (Time.time/2)%PuzzleVars.TwoPi;
 		//gameObject.transform.position = new Vector3((float)Math.Sin(_time)*3,7,-9);
 		//gameObject.transform.eulerAngles = new Vector3(0, 0, (float) (Math.Sin(_time) * 90));
 		//MenuRayCasts();
 	}
 	
-	private void MenuRayCasts(){
-		
-		RaycastHit hit;
-		Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-		Debug.DrawRay(ray.origin,ray.direction,Color.green);
-		
-		//debugText.text = "";
-		if (Physics.Raycast(ray, out hit)) {
-			Transform objectHit = hit.transform;
+	//private void MenuRayCasts(){
+	//	
+	//	RaycastHit hit;
+	//	Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+	//	Debug.DrawRay(ray.origin,ray.direction,Color.green);
+	//	
+	//	//debugText.text = "";
+	//	if (Physics.Raycast(ray, out hit)) {
+	//		Transform objectHit = hit.transform;
 
-			Debug.Log(objectHit);
+	//		Debug.Log(objectHit);
 
 
-			/*
-			Vector3 incomingVec = hit.normal - Vector3.up;
-			if (incomingVec == new Vector3(0, -1, -1)){
+	//		/*
+	//		Vector3 incomingVec = hit.normal - Vector3.up;
+	//		if (incomingVec == new Vector3(0, -1, -1)){
 
-				
-				
-				Renderer rend = hit.transform.GetComponent<Renderer>();
-				MeshCollider meshCollider = hit.collider as MeshCollider;
+	//			
+	//			
+	//			Renderer rend = hit.transform.GetComponent<Renderer>();
+	//			MeshCollider meshCollider = hit.collider as MeshCollider;
 
-				if (rend == null || rend.sharedMaterial == null || rend.sharedMaterial.mainTexture == null || meshCollider == null)
-					return;
+	//			if (rend == null || rend.sharedMaterial == null || rend.sharedMaterial.mainTexture == null || meshCollider == null)
+	//				return;
 
-				//Texture2D tex = rend.material.mainTexture as Texture2D;
-				//Vector2 pixelUV = hit.textureCoord;
-				//debugText.text = pixelUV.ToString();
-				//pixelUV.x *= tex.width;
-				//pixelUV.y *= tex.height;
-				//tex.SetPixel((int)pixelUV.x, (int)pixelUV.y, Color.black);
-				//tex.Apply();
-				
-			
-			}
-			*/
+	//			//Texture2D tex = rend.material.mainTexture as Texture2D;
+	//			//Vector2 pixelUV = hit.textureCoord;
+	//			//debugText.text = pixelUV.ToString();
+	//			//pixelUV.x *= tex.width;
+	//			//pixelUV.y *= tex.height;
+	//			//tex.SetPixel((int)pixelUV.x, (int)pixelUV.y, Color.black);
+	//			//tex.Apply();
+	//			
+	//		
+	//		}
+	//		*/
+//
+	//	}
+	//	
+	//}
 
-		}
-		
-	}
-
-	void menuButtonClicked(){
-		Debug.Log("CLICK");
+	void menuButtonClicked(MenuBlock.ButtonConfig config){
+		Debug.Log("Pressed: "+config.text);
 	}
 }
--- Assets/Scripts/MenuBlock.cs
+++ Assets/Scripts/MenuBlock.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using TMPro;
@@ -8,37 +9,61 @@ public class MenuBlock : MonoBehaviour{
 
 	private Renderer _renderer;
 	private TextMeshPro _text;
+	public ButtonConfig config;
+
+	public struct ButtonConfig{
+		public string text;
+		public float x;
+		public float y;
+
+		public ButtonConfig(string text, float x, float y){
+			this.text = text;
+			this.x = x;
+			this.y = y;
+		}
+	}
 	
-    // Start is called before the first frame update
-    private void Start(){
+    private void Awake(){
 	    _renderer = gameObject.GetComponent<Renderer>();
 	    _renderer.material.color = Color.green;
+	    transform.localPosition = new Vector3(0,0,-0.4f);
+	    transform.localEulerAngles = Vector3.zero;
+	    transform.localScale = Vector3.one;
+    }
+
+    public static MenuBlock createMenuBlock(ButtonConfig config,Transform parent){
+	    var menuBlock = Instantiate(Resources.Load<GameObject>("Prefabs/MenuBlock"), parent, false).GetComponent<MenuBlock>();
+	    menuBlock.initMenuBlock(config);
+	    return menuBlock;
+    }
+
+    private void initMenuBlock(ButtonConfig inConfig){
+	    config = inConfig;
 	    
-	    Debug.Log(this);
-	    
+	    transform.localPosition = new Vector3(config.x,config.y,-0.4f);
+	  
 	    GameObject ga = new GameObject();
 	    _text = ga.AddComponent<TextMeshPro>();
-	    _text.transform.parent = this.transform;
-	    _text.transform.transform.localEulerAngles = new Vector3(0,0,0);
-	    _text.text = "CLICK TO START";
+	    _text.transform.parent = transform;
+	    _text.transform.transform.localEulerAngles = new Vector3(0, 0, 0);
+	    _text.text = config.text;
 	    _text.transform.localScale = Vector3.one;
 	    _text.fontSize = 2;
 	    _text.transform.localPosition  = new Vector3(0,0,-0.3f);
-	    _text.alignment = TextAlignmentOptions.Center;
-
-	    var s = DOTween.Sequence()
-		    .OnComplete(()=> Debug.Log("This will work"));
-	    s.Complete();
-	    
+	    _text.alignment = TextAlignmentOptions.Center; 
     }
 
-    // Update is called once per frame
-    void Update(){
+    private void Update(){
         
     }
 
 	private void OnMouseDown(){
-		gameObject.transform.parent.gameObject.SendMessage("menuButtonClicked",null);
+		gameObject.transform.parent.gameObject.SendMessage("menuButtonClicked",config);
+		_renderer.material.color = Color.red;
+	}
+	
+	private void OnMouseUp(){
+		_renderer.material.color = Color.blue;
 	}
 	
     private void OnMouseEnter(){
